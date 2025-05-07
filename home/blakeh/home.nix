b79cafe1d76ce54e90194e7c8234c70fdb0517ed
Blake Haug <blake@blakehaug.com>
2025-05-07 15:41:34 -0700

    Format home.nix correctly

--- home/blakeh/home.nix
+++ home/blakeh/home.nix
@@ -1,8 +1,11 @@
-home.packages = [];
-programs.bash.enable = true;
-home.file.".config/hypr/hyprland.conf".source = ./user/hyprland.conf;
-home.file.".config/hypr/hyprpaper.conf".source = ./user/hyprpaper.conf;
-home.file."wallpaper.png".source = ./user/wallpaper.png;
+{ config, pkgs, ... }:
 
+{
+  home.packages = [];
+  programs.bash.enable = true;
+  home.file.".config/hypr/hyprland.conf".source = ./user/hyprland.conf;
+  home.file.".config/hypr/hyprpaper.conf".source = ./user/hyprpaper.conf;
+  home.file."wallpaper.png".source = ./user/wallpaper.png;
 
-home.stateVersion = "24.11";
+  home.stateVersion = "24.11";
+}
