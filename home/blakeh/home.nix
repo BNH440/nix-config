c3a314d1fe1b597db70a5c14a6f9fc7ac00b73bc
Blake Haug <blake@blakehaug.com>
2025-05-07 15:42:28 -0700

    Fix file paths

--- home/blakeh/home.nix
+++ home/blakeh/home.nix
@@ -3,9 +3,9 @@
 {
   home.packages = [];
   programs.bash.enable = true;
-  home.file.".config/hypr/hyprland.conf".source = ./user/hyprland.conf;
-  home.file.".config/hypr/hyprpaper.conf".source = ./user/hyprpaper.conf;
-  home.file."wallpaper.png".source = ./user/wallpaper.png;
+  home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
+  home.file.".config/hypr/hyprpaper.conf".source = ./hyprpaper.conf;
+  home.file."wallpaper.png".source = ./wallpaper.png;
 
   home.stateVersion = "24.11";
 }
