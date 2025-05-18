6157bfa5653d5667024cf0cde3801cbc0ba55cca
Blake Haug <blake@blakehaug.com>
2025-05-17 20:48:46 -0700

    Add nvidia drivers

---
+++ hosts/bnh440-pc/nvidia.nix
@@ -0,0 +1,22 @@
+{ config, ... }:
+
+{
+  # Enable OpenGL
+  hardware.opengl.enable = true;
+
+  # Load NVIDIA driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+
+    open = false;
+
+    nvidiaSettings = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+}
