5f00399ea37725a026bf0a554779b8507e82a533
Blake Haug <blake@blakehaug.com>
2025-05-19 15:59:18 -0700

    Fix default apps

--- home/blakeh/home.nix
+++ home/blakeh/home.nix
@@ -28,6 +28,29 @@
     bottles
   ];
 
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/xhtml+xml" = "app.zen_browser.zen.desktop";
+      "text/html" = "app.zen_browser.zen.desktop";
+      "text/xml" = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/ftp" = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/http" = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/https" = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/discord" = "vesktop.desktop";
+      "x-scheme-handler/mailto" = "userapp-Thunderbird-RCWD62.desktop";
+      "message/rfc822" = "userapp-Thunderbird-RCWD62.desktop";
+      "x-scheme-handler/mid" = "userapp-Thunderbird-RCWD62.desktop";
+      "x-scheme-handler/webcal" = "userapp-Thunderbird-YK0M62.desktop";
+      "text/calendar" = "userapp-Thunderbird-YK0M62.desktop";
+      "application/x-extension-ics" = "userapp-Thunderbird-YK0M62.desktop";
+      "x-scheme-handler/webcals" = "userapp-Thunderbird-YK0M62.desktop";
+      "image/webp" = "qimgv.desktop";
+      "image/png" = "qimgv.desktop";
+      "image/jpeg" = "qimgv.desktop";
+    };
+  };
+
   # see configuration.nix for flatpaks
 
   home.stateVersion = "24.11";
